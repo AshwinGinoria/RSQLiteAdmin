4d123a312c6ad3037318c0dd6cb9717db52d68e3
Ashwin Ginoria <ashwinginoria@gmail.com>
2020-03-15 02:18:37 +0530

    Created a Simple R-package using the SearchFunction Created Earlier
    updated .gitignore, Made Design Changesin Search Function, Created RSQLiteAdminAlpha R-package.
    
    Signed-off-by: Ashwin Ginoria <ashwinginoria@gmail.com>

--- RSQLiteAdminAlpha/R/SearchFunction.r
+++ RSQLiteAdminAlpha/R/SearchFunction.r
@@ -1,29 +1,27 @@
 # Only Implemented Equality Type Search Functions Till Now
 
 # Loading Library
-library(DBI)
+require(DBI)
 
 # This Function takes 4 Inputs
 # SQLite Connection variable
 # table_name: the table on which the query is to be performed
 # column_names: the list of columns on which the filter is to be performed
 # values: the list of values each correspoding to column_names(the 3rd argument)
-Query <- function (con, table_name, column_names, values) {
-    
+Search <- function (con, table_name, column_names, values){
+
     string <- paste ('SELECT * FROM', table_name, 'WHERE')
 
     n <- length(column_names)
 
-    if (n != 1) {
-        for (i in 1: (n - 1)) {
+    if (n != 1)
+        for (i in 1: (n - 1))
             string <- paste (string, column_names[i], "=", values[i], "AND")
-        }
-    }
 
     string <- paste (string, column_names[n], "=", values[n])
-    
+
     ans <- dbGetQuery (con, string)
-    
+
     return (ans)
 }
 
