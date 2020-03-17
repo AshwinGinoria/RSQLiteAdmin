60290010b32ea078207ef3233694ff1d1fd2f1ef
Ashwin Ginoria <ashwinginoria@gmail.com>
2020-03-18 02:18:06 +0530

    Made Design Changes
    Added Roxygen2 Comments, documentation and NAMESPACE. Changed Coding Style (a bit). Added Files to git ignore. Changed Documentations and Description.
    
    Signed-off-by: Ashwin Ginoria <ashwinginoria@gmail.com>

--- RSQLiteAdminAlpha/R/SearchFunction.r
+++ RSQLiteAdminAlpha/R/SearchFunction.r
@@ -1,39 +1,43 @@
 # Only Implemented Equality Type Search Functions Till Now
 
-# Loading Library
-require(DBI)
+#' Search via RSQLite (equality).
+#'
+#' This Function Searches the database for values given by the user and returns
+#' the result.It asks the user for the columns and their corresponding values
+#' and then performs an equality search query (like: "SELECT * FROM TABLE1 WHERE
+#' COLUMN1 = VALUE1") on the database Connection provided.
+#'
+#' @param conn Object of Class SQLiteConnection connected to the database in
+#'   which the table is present.
+#' @param table_name String.
+#' @param column_names Vector of column names on which the conditions are to be
+#'   applied.
+#' @param values Vector of conditional values in the same order as column_names.
+#' @return Result of the search query in a dataframe format.
+#' @examples
+#' \code{
+#' conn <- dbConnect (RSQLite::SQLite(), "mtcars.db")
+#' tables <- dbListTables(conn)
+#' ans <- Search(conn, tables[1], c('disp', 'wt'), c(160, 2.620))
+#' }
+#'
+#' @export
+#' @importFrom DBI dbGetQuery
+#'
+Search <- function(conn, table_name, column_names, values) {
 
-# This Function takes 4 Inputs
-# SQLite Connection variable
-# table_name: the table on which the query is to be performed
-# column_names: the list of columns on which the filter is to be performed
-# values: the list of values each correspoding to column_names(the 3rd argument)
-Search <- function (con, table_name, column_names, values){
-
-    string <- paste ('SELECT * FROM', table_name, 'WHERE')
+    string <- paste('SELECT * FROM', table_name, 'WHERE')
 
     n <- length(column_names)
 
-    if (n != 1)
-        for (i in 1: (n - 1))
-            string <- paste (string, column_names[i], "=", values[i], "AND")
+    if (n != 1) {
+        for (i in 1: (n - 1)) {
+            # string <- paste(string, column_names[i], "=", values[i], "AND")
+        }
+    }
+    string <- paste(string, column_names[n], "=", values[n])
 
-    string <- paste (string, column_names[n], "=", values[n])
+    rv <- DBI::dbGetQuery(conn, string)
 
-    ans <- dbGetQuery (con, string)
-
-    return (ans)
+    return (rv)
 }
-
-
-# # Examples
-# # Loading Database
-# con <- dbConnect (RSQLite::SQLite(), "Dummy_Data.db")
-
-# # Getting Table Names
-# tables <- dbListTables(con)
-
-# ans <- Query (con, tables[1], c('disp', 'wt'), c(160, 2.620))
-# print (ans)
-# ans <- Query (con, tables[1], c('disp'), c(160))
-# print (ans)
